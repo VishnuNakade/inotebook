c1431ff6d1c32e740df5a10c67c901141a495b47
Vishnu Nakade <vishnunakade@gmail.com>
2023-12-05 16:54:40 +0530

    add

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', true);
 
-const mongoURI = "mongodb://127.0.0.1:27017/iNotebook?directConnection=true"
+// const DATABASE=process.env.DATABASE
+
+const mongoURI ="mongodb://127.0.0.1:27017/iNotebook?directConnection=true"
 
 const connectToMongo = () => {
     mongoose.connect(mongoURI, () => {
--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ var cors = require('cors')
 
 app.use(cors())
 
-const port = 5000
+const port = process.env.port || 5000
 
 app.use(express.json())
 
